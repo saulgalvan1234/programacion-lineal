c20561367df9f8b510053134158b0c1ecc17a5f3
saulgalvan1234 <47038253+saulgalvan1234@users.noreply.github.com>
2019-02-08 12:28:22 -0600

    archivo en Python de un uso simple de pulp

---
+++ programacionlineal.py
@@ -0,0 +1,9 @@
+from pulp import*
+x = LpVariable("x", 0, 3)
+y = LpVariable("y", 0, 1)
+prob = LpProblem("myProblem", LpMinimize)
+prob += x + y <= 2
+prob += -4*x + y
+status = prob.solve()
+value(x), value(y)
+print(value(x),value(y))
